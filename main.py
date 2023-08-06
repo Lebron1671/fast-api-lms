64b8251a26243b31f0056db816fe96706cf2ccca
Nick Dynin <dynin.nikolaj2010@yandex.ru>
2023-08-06 11:59:46 +0400

    Added base endpoints with storing in list

--- main.py
+++ main.py
@@ -1,7 +1,40 @@
-from fastapi import FastAPI
+from typing import Optional, List
+from fastapi import FastAPI, Path, Query
+from pydantic import BaseModel
 
-app = FastAPI()
+app = FastAPI(
+    title="Fast API LMS",
+    description="LMS for managing students and courses.",
+    version="0.0.1",
+    contact={
+        "name": "Nick",
+        "email": "dynin.nikolaj2010@yandex.ru"
+    },
+    license_info={
+        "name": "Proston_Tech"
+    }
+)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World!!!"}
+users = []
+
+class User(BaseModel):
+    email: str
+    is_active: bool
+    bio: Optional[str] = None
+
+@app.get("/users", response_model=List[User])
+async def get_users():
+    return users
+
+
+@app.post("/users")
+async def create_user(user: User):
+    users.append(user)
+    return "Success"
+
+@app.get("/users/{id}")
+async def get_user(
+        id: int = Path(..., description='The ID of the user you want to retrieve', gt=0),
+        q: str = Query(None, max_length=5)
+):
+    return {"user": users[id], "query": q}
