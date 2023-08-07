8d3e2432ce08e445f093b6b4a4ab2d34a3fe5d88
Nick Dynin <dynin.nikolaj2010@yandex.ru>
2023-08-07 15:13:31 +0400

    Added courses api

--- api/users.py
+++ api/users.py
@@ -1,5 +1,4 @@
-from os import stat
-from typing import Optional, List
+from typing import List
 
 import fastapi
 from fastapi import Depends, HTTPException
@@ -8,6 +7,8 @@ from sqlalchemy.orm import Session
 from db.db_setup import get_db
 from pydantic_schemas.user import UserCreate, User
 from api.utils.users import get_user, get_user_by_email, get_users, create_user
+from api.utils.courses import get_user_courses
+from pydantic_schemas.course import Course
 
 router = fastapi.APIRouter()
 
@@ -32,3 +33,12 @@ async def read_user(user_id: int, db: Session = Depends(get_db)):
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
+
+
+@router.get("/users/{user_id}/courses", response_model=List[Course])
+async def read_user_courses(user_id: int, db: Session = Depends(get_db)):
+    db_user = get_user(db=db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    courses = get_user_courses(db=db, user_id=user_id)
+    return courses
